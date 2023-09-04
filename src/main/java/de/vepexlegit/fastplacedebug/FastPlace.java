1f4e2d5f5935d726d5b1e9a5f42f2952003071c8
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-04 15:32:46 +0400

    Create FastPlace.java

---
+++ src/main/java/de/vepexlegit/fastplacedebug/FastPlace.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.fastplacedebug;
+
+public enum FastPlace {
+    INSTANCE;
+
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
